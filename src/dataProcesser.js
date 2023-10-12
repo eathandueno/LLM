7f308384803eaae0e85435b48b94c258e05c3937
eathandueno <eathandueno@gmail.com>
2023-10-12 11:20:33 -0600

    process multiple json for more training

--- src/dataProcesser.js
+++ src/dataProcesser.js
@@ -7,14 +7,20 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 function processData() {
-  const filePath = path.join(__dirname, '..', 'data', 'responses', 'med_generated_responses.json');
-  const rawData = fs.readFileSync(filePath);
-  const responses = JSON.parse(rawData);
+  const dataDir = path.join(__dirname, '..', 'data', 'responses');
+  const files = fs.readdirSync(dataDir).filter(file => file.endsWith('.json'));
 
   const processedData = {};
-  for (const [prompt, response] of Object.entries(responses)) {
-    let cleanedResponse = response.replace(/\n/g, ' ').replace(/[.,!?]/g, ' ');
-    processedData[prompt] = cleanedResponse.match(/\b\w+\b/g);
+
+  for (const file of files) {
+    const filePath = path.join(dataDir, file);
+    const rawData = fs.readFileSync(filePath);
+    const responses = JSON.parse(rawData);
+
+    for (const [prompt, response] of Object.entries(responses)) {
+      let cleanedResponse = response.replace(/\n/g, ' ').replace(/[.,!?]/g, ' ');
+      processedData[prompt] = cleanedResponse.match(/\b\w+\b/g);
+    }
   }
 
   const processedFilePath = path.join(__dirname, '..', 'data', 'processed', 'med_processed_data.json');
